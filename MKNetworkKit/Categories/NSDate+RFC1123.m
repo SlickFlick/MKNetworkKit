5d0abc77d65e35398c6185c7a46a20af2db9d36b
Walter Fettich <wfettich@gmail.com>
2012-10-17 20:57:46 +0300

    modified date string

--- MKNetworkKit/Categories/NSDate+RFC1123.m
+++ MKNetworkKit/Categories/NSDate+RFC1123.m
@@ -16,48 +16,63 @@
     if(value_ == nil)
         return nil;    
     
-    __strong static NSDateFormatter *rfc1123 = nil;
-    if (!rfc1123) {
-        static dispatch_once_t oncePredicate;
-        dispatch_once(&oncePredicate, ^{
-            rfc1123 = [[NSDateFormatter alloc] init];
-            rfc1123.locale = [[NSLocale alloc] initWithLocaleIdentifier:@"en_US"];
-            rfc1123.timeZone = [NSTimeZone timeZoneWithAbbreviation:@"GMT"];
-            rfc1123.dateFormat = @"EEE',' dd MMM yyyy HH':'mm':'ss z";
-        });
+    if ([value_ isKindOfClass:[NSNumber class]])
+    {        
+        NSNumber* dateNr = (NSNumber*)value_;
+        NSTimeInterval _interval=[dateNr intValue];
+        NSDate* sourceDate = [NSDate dateWithTimeIntervalSince1970:_interval];
+        NSTimeZone* destinationTimeZone = [NSTimeZone systemTimeZone];
+        NSInteger destinationGMTOffset = [destinationTimeZone secondsFromGMTForDate:sourceDate];
+        NSTimeInterval interval = destinationGMTOffset;
+        NSDate* date = [[NSDate alloc] initWithTimeInterval:interval sinceDate:sourceDate];
+        
+        return date;
     }
-    NSDate *ret = [rfc1123 dateFromString:value_];
-    if(ret != nil)
-        return ret;
-    
-    static NSDateFormatter *rfc850 = nil;
-    if(!rfc850)
-    {
-        static dispatch_once_t oncePredicate;
-        dispatch_once(&oncePredicate, ^{
-            rfc850 = [[NSDateFormatter alloc] init];
-            rfc850.locale = rfc1123.locale;
-            rfc850.timeZone = rfc1123.timeZone;
-            rfc850.dateFormat = @"EEEE',' dd'-'MMM'-'yy HH':'mm':'ss z";
-        });
+    else
+    {    
+        __strong static NSDateFormatter *rfc1123 = nil;
+        if (!rfc1123) {
+            static dispatch_once_t oncePredicate;
+            dispatch_once(&oncePredicate, ^{
+                rfc1123 = [[NSDateFormatter alloc] init];
+                rfc1123.locale = [[NSLocale alloc] initWithLocaleIdentifier:@"en_US"];
+                rfc1123.timeZone = [NSTimeZone timeZoneWithAbbreviation:@"GMT"];
+                rfc1123.dateFormat = @"EEE',' dd MMM yyyy HH':'mm':'ss z";
+            });
+        }
+        NSDate *ret = [rfc1123 dateFromString:value_];
+        if(ret != nil)
+            return ret;
+        
+        static NSDateFormatter *rfc850 = nil;
+        if(!rfc850)
+        {
+            static dispatch_once_t oncePredicate;
+            dispatch_once(&oncePredicate, ^{
+                rfc850 = [[NSDateFormatter alloc] init];
+                rfc850.locale = rfc1123.locale;
+                rfc850.timeZone = rfc1123.timeZone;
+                rfc850.dateFormat = @"EEEE',' dd'-'MMM'-'yy HH':'mm':'ss z";
+            });
+        }
+        ret = [rfc850 dateFromString:value_];
+        if(ret != nil)
+            return ret;
+        
+        static NSDateFormatter *asctime = nil;
+        if(!asctime)
+        {
+            static dispatch_once_t oncePredicate;
+            dispatch_once(&oncePredicate, ^{
+                
+                asctime = [[NSDateFormatter alloc] init];
+                asctime.locale = rfc1123.locale;
+                asctime.timeZone = rfc1123.timeZone;
+                asctime.dateFormat = @"EEE MMM d HH':'mm':'ss yyyy";
+            });
+        }
+        return [asctime dateFromString:value_];
     }
-    ret = [rfc850 dateFromString:value_];
-    if(ret != nil)
-        return ret;
-    
-    static NSDateFormatter *asctime = nil;
-    if(!asctime)
-    {
-        static dispatch_once_t oncePredicate;
-        dispatch_once(&oncePredicate, ^{
-            
-            asctime = [[NSDateFormatter alloc] init];
-            asctime.locale = rfc1123.locale;
-            asctime.timeZone = rfc1123.timeZone;
-            asctime.dateFormat = @"EEE MMM d HH':'mm':'ss yyyy";
-        });
-    }
-    return [asctime dateFromString:value_];
 }
 
 -(NSString*)rfc1123String
